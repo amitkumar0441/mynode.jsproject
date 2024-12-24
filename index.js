ffc8fdf19b6dc8d0859012cff01d54afe57c6fbc
amit <amit@remotemachine.localdomain>
2024-12-24 13:18:36 +0530

    source code has been pushed

---
+++ index.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Welcome to CLOUD ASEEM  & Credit with Thanks to - A Monk in Cloud'); //write a response to the client
+  res.end(); //end the response
+}).listen(3000); //the server object listens on port 3000 , mention your port specific
+
